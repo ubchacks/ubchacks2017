78f2cde1b694f337cb029e1b86f4a13b7c12979d
charmaine <charmaine@Charmaine.hitronhub.home>
2017-08-02 00:59:49 -0700

    added logos, edited text, some stylistic changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+
+
 $(document).ready(function(){
-  
-  
+
+
+
   //------------------------------------//
   //Navbar//
   //------------------------------------//
@@ -16,20 +19,20 @@ $(document).ready(function(){
     			}
     		}
     	});
-  
-  
+
+
   //------------------------------------//
   //Scroll To//
   //------------------------------------//
-  $(".scroll").click(function(event){		
+  $(".scroll").click(function(event){
   	event.preventDefault();
   	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-  	
+
   });
-  
+
   //------------------------------------//
   //Wow Animation//
-  //------------------------------------// 
+  //------------------------------------//
   wow = new WOW(
         {
           boxClass:     'wow',      // animated element css class (default is wow)
@@ -41,5 +44,5 @@ $(document).ready(function(){
       wow.init();
 
 
-	
+
 });
